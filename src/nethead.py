d0f3178c275667a68aae75260446e663bd5a25f0
Ken Bannister <kb2ma@runbox.com>
2020-04-02 11:03:49 -0400

    Convert display from data table to line graph

--- src/nethead.py
+++ src/nethead.py
@@ -6,45 +6,48 @@ from app import app
 from app import server
 from app import applog
 
+from datetime import datetime
 import json
 import numpy as np
 import pandas as pd
+import plotly.express as px
 import requests
 
 """Main/Top view for NetHead UI.
 """
 
 def serve_layout():
+    df = _collect_data()
+    fig = px.line(df, x="time", y="temp")
+
     return html.Div([
-        dash_table.DataTable(id='data-table', 
-            columns=[{"name": i, "id": i} for i in df.columns],
-            data=df.to_dict("rows")
-        )
+        dcc.Graph(figure=fig),
+
+        #dash_table.DataTable(id='data-table', 
+        #    columns=[{"name": i, "id": i} for i in df.columns],
+        #    data=df.to_dict("rows")
+        #)
     ])
 
 def _collect_data():
-    """Collects data from Graphite.
+    """Collects data from Graphite into a dataframe.
     """
     url = 'http://localhost:8089/render'
     params = {
-        'target': 'time.3303.0',
-        'from': 'now-30min',
+        'target': 'temp.3303.0',
+        'from': 'now-6hr',
         'format': 'json'
     }
 
     data = json.loads(requests.get(url, params=params).text)
-    #print(data)
     series = np.array(data[0]['datapoints'])
-    #print(series)
-    #print(series[:, 0])
-    #print(series[:, 1])
 
-    return pd.DataFrame(series,
+    df = pd.DataFrame(series,
                         index=series[:, 1],
                         columns=['temp', 'time'])
+    # time as hours:minutes
+    df['time'] = df['time'].apply(lambda x: datetime.fromtimestamp(x).strftime('%H:%M'))
+    return df
 
-
-df = _collect_data()
-#print(df)
-
+# uses the function to dynamically rebuild page on refresh
 app.layout = serve_layout
