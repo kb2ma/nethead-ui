58a55a55541019b935021a59f01a8116d6be295e
Ken Bannister <kb2ma@runbox.com>
2020-06-21 06:34:20 -0400

    Refresh table once per minute.

--- src/devices.py
+++ src/devices.py
@@ -3,12 +3,13 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
 from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 
 from app import app, applog, db
 
 import pandas as pd
 
-"""Device list display"""
+"""Device list display; automatic refresh"""
 
 def _gen_registration_df():
     """Generate Pandas dataframe from registration query."""
@@ -26,26 +27,43 @@ def _gen_table_cols(col_ids):
                     like "s.serial_number", as in the SQL select statement
                     -- except for derived column values which must literally
                     use "alias." plus the name.
-    :return: List of dictionaries, where ach contains an 'id' and a 'name' key
+    :return: List of dictionaries, where each contains an 'id' and a 'name' key
              for a Dash DataTable.
     """
     col_list = []
     for col in col_ids:
         split_col = col.partition('.')
         if split_col[0] == 'alias':
-            col_list.append({'id' : 'alias_{}'.format(split_col[2]), 'name' : split_col[2]})
+            col_list.append({'id' : 'alias_{}'.format(split_col[2]),
+                             'name' : split_col[2]})
         else:
             col_list.append({'id' : split_col[2], 'name' : split_col[2]})
 
     return col_list
 
+# invariant, so just create once
+columns = _gen_table_cols(['r.endpoint', 'r.reg_id', 'r.identity', 'r.lifetime',
+                           'alias.last_update'])
 
 def page_layout():
     df = _gen_registration_df()
     if not df.empty:
         return [
-            dash_table.DataTable(id='reg-table',
-                columns=_gen_table_cols(['r.endpoint', 'r.reg_id', 'r.identity', 'r.lifetime', 'alias.last_update']),
-                data=(df.to_dict('records'))
-        )]
+            dash_table.DataTable(id='reg-table', columns=columns,
+                                 data=df.to_dict('records')),
+            dcc.Interval(
+                id='reg-refresh',
+                interval=60*1000, # in milliseconds
+                n_intervals=0)
+        ]
     return 'No devices found'
+
+
+@app.callback(Output('reg-table', 'data'),
+              [Input('reg-refresh', 'n_intervals')])
+def updateTable(n):
+    df = _gen_registration_df()
+    if not df.empty:
+        return df.to_dict('records')
+    else:
+        raise PreventUpdate
